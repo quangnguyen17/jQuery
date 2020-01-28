6ec10ee547aaf8679d8dfb3a3b4b21de56446df2
Quang Nguyen <quangnguyen@Quangs-MacBook-Pro.local>
2020-01-28 10:06:41 -0800

    jquery

--- jquery-functions/jquery-functions.js
+++ jquery-functions/jquery-functions.js
@@ -9,11 +9,11 @@ function setupUI() {
     var html = "";
 
     for (var index = 0; index < functions.length; index++) {
-        html += `<div id="${index}" class="col-6 m-0 p-5">
+        html += `<div id="${index}" class="col-4 m-0 p-5">
         <button class="btn btn-outline-dark my-5 mr-5">${functions[index].title}</button>
         <div class="text-box d-inline-block align-middle">
             <h1>${functions[index].title}</h1>
-            <p>Practice using ${functions[index].title}</p>
+            <p>Example using ${functions[index].title}</p>
             </div>
         </div>`;
     }
@@ -21,21 +21,42 @@ function setupUI() {
     $('.row').append(html);
 }
 
+// Document is finished loading! :)
 $(document).ready(function () {
     setupUI();
 
+    // This feature is currently not working. :(
+    $('#reset-all').click(function () {
+        console.log("#reset-all clicked.");
+        $('.row').html = "";
+        setupUI();
+    });
+
+    // .click, .hide, .show, .toggle
     $('#0 button').click(function () {
-        $('#0 p').text("Button has been clicked!").addClass('red');
-        console.log("Button has been clicked!");
+        $('#0 p').text("Button has been clicked.").addClass('red');
+        console.log("Button has been clicked.");
     });
 
     $('#1 button').click(function () {
         $('#1 p').hide();
-        console.log("Paragraph is now hidden!");
+        console.log("Paragraph is now hidden.");
     });
 
-    $('#1 button').click(function () {
-        $('#1 p').hide();
-        console.log("Paragraph is now hidden!");
+    $('#2 p').hide();
+    $('#2 button').click(function () {
+        $('#2 p').show();
+        console.log("Paragraph is now shown.");
+    });
+
+    $('#3 button').click(function () {
+        $('#3 p').toggle();
+        console.log("Paragraph is now being toggled to show / hide.");
+    });
+
+    // .slideDown, .slideUp, .slideToggle, .fadeIn, .fadeOut
+    $('#4 button').click(function () {
+        $('#4 p').slideDown();
+        console.log("Paragraph is now being toggled to show / hide.");
     });
 });
